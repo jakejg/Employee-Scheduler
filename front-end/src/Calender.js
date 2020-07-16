e6c06f4a5dbdeac4118d6888a36f9cec9dc00813
Jake <jakefrisbee@gmail.com>
2020-07-16 15:11:37 -0700

    testing calendar library

--- front-end/src/Calender.js
+++ front-end/src/Calender.js
@@ -5,7 +5,7 @@ import 'react-calendar-timeline/lib/Timeline.css'
 import moment from 'moment'
 
 const Calender = () => {
-const groups = [{ id: 1, title: 'group 1' }, { id: 2, title: 'group 2' }]
+const groups = [{ id: 1, title: 'group 1' }, { id: 2, title: 'group 2' }, { id: 3, title: 'group 2' }, { id: 4, title: 'group 2' }, { id: 5, title: 'group 2' }]
 
 const items = [
   {
