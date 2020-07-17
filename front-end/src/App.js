bbb41a50d2b34c882ca392135523001ec03fb2cf
Jake <jakefrisbee@gmail.com>
2020-07-16 21:15:23 -0700

    added material ui nav bar

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import './App.css';
 import Calender from './Calender';
+import NavBar from './NavBar';
 
 function App() {
 
-  return (
-    <div className="App">
-     
-      <Calender />
- 
-     
-    </div>
-  );
+    return (
+        <div className="App">
+            <NavBar/>
+            <Calender />        
+
+        </div>
+        );
 }
 
 export default App;
