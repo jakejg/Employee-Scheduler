5467ad4b69f454782546e43ba728c7e145e6bfa8
Jake <jakefrisbee@gmail.com>
2020-07-20 10:14:17 -0700

    added sidebar to dashboard ui

--- front-end/src/components/Dashboard.js
+++ front-end/src/components/Dashboard.js
@@ -1,16 +1,9 @@
 import React from 'react';
 import ItemList from './ItemList';
-// import Drawer from '@material-ui/core/Drawer';
-// import Divider from '@material-ui/core/Divider';
-// import List from '@material-ui/core/List';
-// import Toolbar from '@material-ui/core/Toolbar'
-// import ListItem from '@material-ui/core/ListItem';
-// import ListItemIcon from '@material-ui/core/ListItemIcon';
-// import ListItemText from '@material-ui/core/ListItemText';
-// import InboxIcon from '@material-ui/icons/MoveToInbox';
-// import MailIcon from '@material-ui/icons/Mail';
-
-
+import Sidebar from './Sidebar';
+import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
 
 
 
@@ -18,36 +11,22 @@ const Dashboard = () => {
 
     return (
         <div className="Dashboard">
-            <div>
-            {/* <Drawer variant="permanent" >
-                <Toolbar />
-                    <div>
-                        <List>
-                          {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-                            <ListItem button key={text}>
-                              <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-                              <ListItemText primary={text} />
-                            </ListItem>
-                          ))}
-                        </List>
-                        <Divider />
-                        <List>
-                            {['All mail', 'Trash', 'Spam'].map((text, index) => (
-                            <ListItem button key={text}>
-                                <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-                                <ListItemText primary={text} />
-                            </ListItem>
-                            ))}
-                        </List>
-                    </div>
-                <Toolbar />
-            </Drawer> */}
-            </div>
-            <div>
-            <ItemList type='jobs' name='title' />
-            <ItemList type='staff' name='first_name' />
-            </div>
+            <Grid container spacing={3}>
+                <Grid item xs>
+                    <Sidebar />
+                </Grid>
+                <Grid item xs>
+                    <Paper>
+                    <ItemList type='jobs' name='title' />
+                    </Paper>
+                </Grid>
+                <Grid item xs>
+                    <Paper>
+                        <ItemList type='staff' name='first_name' />
+                    </Paper>
+                </Grid>
                
+             </Grid>
         </div>
         );
 }
--- front-end/src/components/NavBar.js
+++ front-end/src/components/NavBar.js
@@ -9,32 +9,41 @@ import MenuIcon from '@material-ui/icons/Menu';
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    flexGrow: 1,
+    display: 'flex',
   },
-  menuButton: {
-    marginRight: theme.spacing(2),
+  appBar: {
+    zIndex: theme.zIndex.drawer + 1,
   },
-  title: {
+  content: {
     flexGrow: 1,
+    padding: theme.spacing(3),
   },
 }));
 
 export default function ButtonAppBar() {
   const classes = useStyles();
 
+
   return (
     <div className={classes.root}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+      
+  <AppBar position="sticky" className={classes.appBar}>
+  <Toolbar>
+  <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
             <MenuIcon />
           </IconButton>
-          <Typography variant="h6" className={classes.title}>
+          <Typography variant="h6" noWrap className={classes.title}>
             News
           </Typography>
-          <Button color="inherit">Login</Button>
+    <Button color="inherit">Login</Button>
+  </Toolbar>
+</AppBar>
+      {/* <AppBar position="fixed">
+        <Toolbar>
+         
+          
         </Toolbar>
-      </AppBar>
+      </AppBar> */}
     </div>
   );
 }
