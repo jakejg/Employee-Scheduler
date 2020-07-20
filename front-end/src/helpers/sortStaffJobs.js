90c56bf60eb5f5b31a23aec5c4ed9f051136958b
Jake <jakefrisbee@gmail.com>
2020-07-20 09:33:25 -0700

    created function to sort staff jobs by date

---
+++ front-end/src/helpers/sortStaffJobs.js
@@ -0,0 +1,27 @@
+import moment from 'moment';
+
+/* Takes an array of all jobs associated with a staff, 
+sorts into jobs worked, jobs scheduled for, and currently working,
+returns an object with two arrays of job ids and current job */
+
+function sortJobs(jobs) {
+    let past = [];
+    let future = [];
+    let current;
+
+    for (let job of jobs) {
+        if (moment(job.end_date) < moment()) {
+            past.push(job.id)
+        }
+        else if (moment(job.start_date) > moment()) {
+            future.push(job.id)
+        }
+        else {
+            current = job.id;
+        }
+    }
+
+    return {past, future, current}
+}
+
+export default sortJobs;
