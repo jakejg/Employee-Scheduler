77b760ab8ecf889d9b792be2a988e7b31b7c3f20
Jake <jakefrisbee@gmail.com>
2020-07-28 12:30:24 -0700

    set up demo version button

--- front-end/src/components/Home.js
+++ front-end/src/components/Home.js
@@ -8,20 +8,33 @@ import {
 import AddForm from './AddForm';
 import {register} from '../actions/authentication';
 import {login} from '../actions/authentication';
+import { useDispatch } from 'react-redux';
+import { useHistory } from 'react-router-dom';
 
 
 const Home = () => {
     const [dialog, setDialog] = useState({isOpen: false, type:""}) 
+    const dispatch = useDispatch();
+    const history = useHistory();
 
 
     const registerFields = ['Company Name', 'Username', 'Password', 'First Name','Last Name'];
     const loginFields = ['Username', 'Password'];
+
+    const loginDemo = async() => {
+        const demoUser = {
+            username: "Demo",
+            password: "m"
+        }
+        await dispatch(login(demoUser))
+        history.push('/dashboard')
+    }
    
     return (
         <div>
             <Button variant='contained' onClick={()=> setDialog(dialog => ({isOpen: true, type: "Register"}))}>Register your company</Button>
             <Button variant='contained' onClick={()=> setDialog(dialog => ({isOpen: true, type: "Login"}))}>Login</Button>
-            <Button variant='contained'>View Demo Version</Button>
+            <Button variant='contained' onClick={loginDemo}>View Demo Version</Button>
 
             <Dialog open={dialog.isOpen} onClose={() => setDialog(dialog => ({isOpen: false, type: ""}))} fullWidth={true}>
                 <DialogTitle><Box textAlign='center' >Add Company </Box></DialogTitle>
