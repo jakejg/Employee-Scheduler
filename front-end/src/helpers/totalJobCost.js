d59e41fc61a972cf558ea170918f7cbae2050248
Jake <jakefrisbee@gmail.com>
2020-08-09 13:17:08 -0700

    changed display for total trip cost to show commas in large numbers

--- front-end/src/helpers/totalJobCost.js
+++ front-end/src/helpers/totalJobCost.js
@@ -4,7 +4,7 @@ function getTotalCost(job){
     // get length of trip in days
     let length = job.end_time.diff(job.start_time, 'days');
     
-    return job.staff.reduce((wages, staff) => { return wages + staff.current_wage * length}, 0);
+    return job.staff.reduce((wages, staff) => { return wages + staff.current_wage * length}, 0).toLocaleString();
 }
 
 export default getTotalCost
