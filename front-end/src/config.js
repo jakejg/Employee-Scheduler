0188166b5a1917d203b36a651abd5cd3c5e8ce4f
Jake <jakefrisbee@gmail.com>
2020-08-21 10:27:17 -0700

    changed base_url to react_app_base_url for production'

--- front-end/src/config.js
+++ front-end/src/config.js
@@ -1,3 +1,2 @@
-console.log(process.env.BASE_URL)
-export const BASE_URL = process.env.BASE_URL || `http://localhost:5000`;
+export const BASE_URL = process.env.REACT_APP_BASE_URL || `http://localhost:5000`;
 export const drawerWidth = 240;
